ec20a2e6cc7092ae5b085ef06753bddbbaa71064
Suhail Shaik <shaiksuhail74818@gmail.com>
2025-06-23 12:53:43 +0530

    updated4

---
+++ HackerRank/math/m2_find_angle_mbc.py
@@ -0,0 +1,11 @@
+import math
+lenght_of_side_AB =int(input())
+lenght_of_side_BC =int(input())
+
+lenght_of_side_AC=math.sqrt(lenght_of_side_AB**2+lenght_of_side_BC**2)
+lenght_of_side_MC=lenght_of_side_AC/2
+#print(lenght_of_side_MC)
+
+x=math.atan(lenght_of_side_MC/lenght_of_side_BC)
+y=round(math.degrees(x))
+print(y)
