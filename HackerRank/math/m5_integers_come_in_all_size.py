4368de762883d31d9e9f28a1230f88243a353597
Suhail Shaik <shaiksuhail74818@gmail.com>
2025-06-24 12:59:43 +0530

    updated4

---
+++ HackerRank/math/m5_integers_come_in_all_size.py
@@ -0,0 +1,9 @@
+import math
+integer_a = int(input())
+integer_b = int(input())
+integer_c = int(input())
+integer_d = int(input())
+result_1 = math.pow(integer_a,integer_b)
+result_2 = math.pow(integer_c,integer_d)
+result=result_1+result_2
+print(int(result))
